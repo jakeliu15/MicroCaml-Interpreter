bdc3e6e036197898ab23029852f7b65fbb57c4ca
jakeliu15 <91630668+jakeliu15@users.noreply.github.com>
2024-04-06 19:54:36 -0400

    h

--- src/lexer.ml
+++ src/lexer.ml
@@ -1,94 +1,116 @@
 open Types
 open Str
 
+exception InvalidInputException
+
 let tokenize input =
   let rec helper input tokens =
-    let string_startswith s prefix =
-      let len = String.length prefix in
-      if String.length s < len then false
-      else String.sub s 0 len = prefix
+  
+    let ignore_spaces input =
+      if string_match (regexp "^[ \n\t]+") input 0 then
+        string_after input (match_end ())
+      else
+        input
     in
-    
-    if input = "" then
-      List.rev tokens  
+    if input = "" || string_match (regexp "^[ \n\t]+$") input 0 then
+      List.rev tokens
     else
-      let input = if string_match (regexp "^[ \n\t]+") input 0 then
-                    replace_matched "" input  
-                  else
-                    input
-      in
+      let input = ignore_spaces input in  
+
       if string_match (regexp "^\\(-?[0-9]+\\)") input 0 then
         let matched = matched_group 1 input in
-        let n = if matched.[0] = '(' then int_of_string (String.sub matched 1 ((String.length matched) - 2)) else int_of_string matched
+        let n = int_of_string matched in
+        helper (ignore_spaces (string_after input (String.length matched))) (Tok_Int n :: tokens)
+      else if string_match (regexp "^\\((-?[0-9]+)\\)") input 0 then
+        let matched = matched_group 1 input in
+        let n = 
+          if String.length matched > 0 && matched.[0] = '(' then
+            int_of_string (String.sub matched 1 (String.length matched - 2))
+          else
+            int_of_string matched
         in
-        helper (string_after input (String.length matched)) (Tok_Int n :: tokens)
+        helper (ignore_spaces (string_after input (String.length matched))) (Tok_Int n :: tokens)
       else if string_match (regexp "^\\\"[^\"]*\\\"") input 0 then
         let matched = matched_string input in
-        let str_value = String.sub matched 1 ((String.length matched) - 2) in 
-        helper (string_after input (String.length matched)) (Tok_String str_value :: tokens)
+        let str_value = String.sub matched 1 ((String.length matched) - 2) in
+        helper (ignore_spaces (string_after input (String.length matched))) (Tok_String str_value :: tokens)
+      else if string_match (regexp "^true") input 0 then
+        let bool = matched_string input in
+        let token = Tok_Bool true in
+        helper (ignore_spaces (string_after input (String.length bool))) (token :: tokens)
+      else if string_match (regexp "^false") input 0 then
+        let bool = matched_string input in
+        let token = Tok_Bool false in
+        helper (ignore_spaces (string_after input (String.length bool))) (token :: tokens)
+      else if string_match (regexp "^not\\b") input 0 then
+        helper (ignore_spaces (string_after input 3)) (Tok_Not :: tokens)
+      else if string_match (regexp "^if\\b") input 0 then
+        helper (ignore_spaces (string_after input 2)) (Tok_If :: tokens)
+      else if string_match (regexp "^then\\b") input 0 then
+        helper (ignore_spaces (string_after input 4)) (Tok_Then :: tokens)
+      else if string_match (regexp "^else\\b") input 0 then
+        helper (ignore_spaces (string_after input 4)) (Tok_Else :: tokens)
+      else if string_match (regexp "^let\\b") input 0 then
+        helper (ignore_spaces (string_after input 3)) (Tok_Let :: tokens)
+      else if string_match (regexp "^def\\b") input 0 then
+        helper (ignore_spaces (string_after input 3)) (Tok_Def :: tokens)
+      else if string_match (regexp "^in\\b") input 0 then
+        helper (ignore_spaces (string_after input 2)) (Tok_In :: tokens)
+      else if string_match (regexp "^rec\\b") input 0 then
+        helper (ignore_spaces (string_after input 3)) (Tok_Rec :: tokens)
+      else if string_match (regexp "^fun\\b") input 0 then
+        helper (ignore_spaces (string_after input 3)) (Tok_Fun :: tokens)
       else if string_match (regexp "^[a-zA-Z][a-zA-Z0-9]*") input 0 then
         let matched = matched_string input in
-        helper (string_after input (String.length matched)) (Tok_ID matched :: tokens)
-      else if string_match (regexp "^(true|false)") input 0 then
-        let bool = matched_string input in
-        let token = if bool = "true" then Tok_Bool true else Tok_Bool false in
-        helper (string_after input (String.length bool)) (token :: tokens)
+        helper (ignore_spaces (string_after input (String.length matched))) (Tok_ID matched :: tokens)
+      
       else match input.[0] with
-        | '(' -> helper (string_after input 1) (Tok_LParen :: tokens)
-        | ')' -> helper (string_after input 1) (Tok_RParen :: tokens)
-        | '{' -> helper (string_after input 1) (Tok_LCurly :: tokens)
-        | '}' -> helper (string_after input 1) (Tok_RCurly :: tokens)
-        | '.' -> helper (string_after input 1) (Tok_Dot :: tokens)
-        | '+' -> helper (string_after input 1) (Tok_Add :: tokens)
-        | '-' -> 
-            if string_startswith input "->" then
-              helper (string_after input 2) (Tok_Arrow :: tokens)
+        | '(' -> helper (ignore_spaces (string_after input 1)) (Tok_LParen :: tokens)
+        | ')' -> helper (ignore_spaces (string_after input 1)) (Tok_RParen :: tokens)
+        | '{' -> helper (ignore_spaces (string_after input 1)) (Tok_LCurly :: tokens)
+        | '}' -> helper (ignore_spaces (string_after input 1)) (Tok_RCurly :: tokens)
+        | '.' -> helper (ignore_spaces (string_after input 1)) (Tok_Dot :: tokens)
+        | '+' -> helper (ignore_spaces (string_after input 1)) (Tok_Add :: tokens)
+        | '-' ->
+            if Str.string_match (Str.regexp "^->") input 0 then
+              helper (ignore_spaces (string_after input 2)) (Tok_Arrow :: tokens)
             else
-              helper (string_after input 1) (Tok_Sub :: tokens)
-        | '*' -> helper (string_after input 1) (Tok_Mult :: tokens)
-        | '/' -> helper (string_after input 1) (Tok_Div :: tokens)
-        | '^' -> helper (string_after input 1) (Tok_Concat :: tokens)
-        | ';' -> 
-            if string_startswith input ";;" then
-              helper (string_after input 2) (Tok_DoubleSemi :: tokens)
+              helper (ignore_spaces (string_after input 1)) (Tok_Sub :: tokens)
+        | '*' -> helper (ignore_spaces (string_after input 1)) (Tok_Mult :: tokens)
+        | '/' -> helper (ignore_spaces (string_after input 1)) (Tok_Div :: tokens)
+        | '^' -> helper (ignore_spaces (string_after input 1)) (Tok_Concat :: tokens)
+        | ';' ->
+            if Str.string_match (Str.regexp "^;;") input 0 then
+              helper (ignore_spaces (string_after input 2)) (Tok_DoubleSemi :: tokens)
             else
-              helper (string_after input 1) (Tok_Semi :: tokens)
-        | _ -> 
-           if string_match (regexp "^==") input 0 then
-        helper (string_after input 2) (Tok_Equal :: tokens)
-      else if string_match (regexp "^<>") input 0 then
-        helper (string_after input 2) (Tok_NotEqual :: tokens)
-      else if string_match (regexp "^>=") input 0 then
-        helper (string_after input 2) (Tok_GreaterEqual :: tokens)
-      else if string_match (regexp "^<=") input 0 then
-        helper (string_after input 2) (Tok_LessEqual :: tokens)
-      else if string_match (regexp "^>") input 0 then
-        helper (string_after input 1) (Tok_Greater :: tokens)
-      else if string_match (regexp "^<") input 0 then
-        helper (string_after input 1) (Tok_Less :: tokens)
-      else if string_startswith input "||" then
-        helper (string_after input 2) (Tok_Or :: tokens)
-      else if string_startswith input "&&" then
-        helper (string_after input 2) (Tok_And :: tokens)
-      else if string_match (regexp "^not\\([ \n\t;.,(){}]\\|$\\)") input 0 then
-        helper (string_after input 3) (Tok_Not :: tokens)
-      else if string_match (regexp "^if\\([ \n\t;.,(){}]\\|$\\)") input 0 then
-        helper (string_after input 2) (Tok_If :: tokens)
-      else if string_match (regexp "^then\\([ \n\t;.,(){}]\\|$\\)") input 0 then
-        helper (string_after input 4) (Tok_Then :: tokens)
-      else if string_match (regexp "^else\\([ \n\t;.,(){}]\\|$\\)") input 0 then
-        helper (string_after input 4) (Tok_Else :: tokens)
-      else if string_match (regexp "^let\\([ \n\t;.,(){}]\\|$\\)") input 0 then
-        helper (string_after input 3) (Tok_Let :: tokens)
-      else if string_match (regexp "^def\\([ \n\t;.,(){}]\\|$\\)") input 0 then
-        helper (string_after input 3) (Tok_Def :: tokens)
-      else if string_match (regexp "^in\\([ \n\t;.,(){}]\\|$\\)") input 0 then
-        helper (string_after input 2) (Tok_In :: tokens)
-      else if string_match (regexp "^rec\\([ \n\t;.,(){}]\\|$\\)") input 0 then
-        helper (string_after input 3) (Tok_Rec :: tokens)
-      else if string_match (regexp "^fun\\([ \n\t;.,(){}]\\|$\\)") input 0 then
-        helper (string_after input 3) (Tok_Fun :: tokens)
-      else
-          failwith "Unknown token"
+              helper (ignore_spaces (string_after input 1)) (Tok_Semi :: tokens)
+        | '=' ->
+            if Str.string_match (Str.regexp "^=") input 0 then
+              helper (ignore_spaces (string_after input 2)) (Tok_Equal :: tokens)
+            else
+              raise InvalidInputException
+        | '<' ->
+            if Str.string_match (Str.regexp "^<=") input 0 then
+              helper (ignore_spaces (string_after input 2)) (Tok_LessEqual :: tokens)
+            else if Str.string_match (Str.regexp "^<>") input 0 then
+              helper (ignore_spaces (string_after input 2)) (Tok_NotEqual :: tokens)
+            else
+              helper (ignore_spaces (string_after input 1)) (Tok_Less :: tokens)
+        | '>' ->
+            if Str.string_match (Str.regexp "^>=") input 0 then
+              helper (ignore_spaces (string_after input 2)) (Tok_GreaterEqual :: tokens)
+            else
+              helper (ignore_spaces (string_after input 1)) (Tok_Greater :: tokens)
+        | '&' ->
+            if Str.string_match (Str.regexp "^&&") input 0 then
+              helper (ignore_spaces (string_after input 2)) (Tok_And :: tokens)
+            else
+              raise InvalidInputException
+        | '|' ->
+            if Str.string_match (Str.regexp "^||") input 0 then
+              helper (ignore_spaces (string_after input 2)) (Tok_Or :: tokens)
+            else
+              raise InvalidInputException
+        | _ -> raise InvalidInputException
   in
   helper input []
--- src/parser.ml
+++ src/parser.ml
@@ -33,8 +33,230 @@ let rec lookahead_many (toks : token list) (n : int) =
 
 (* Part 2: Parsing expressions *)
 
-let rec parse_expr toks = failwith "unimplemented"
+exception InvalidInputException of string
 
-(* Part 3: Parsing mutop *)
+let rec parse_expr toks =
+  match lookahead toks with
+  | None -> raise (InvalidInputException "Expected expression")
+  | Some tok ->
+    match tok with
+    | Tok_Let ->
+      let toks = match_token toks Tok_Let in
+      let toks, recursive = parse_recursion toks in
+      let toks, var = parse_id toks in
+      let toks = match_token toks Tok_Equal in
+      let toks, expr1 = parse_expr toks in
+      (match lookahead toks with
+       | Some Tok_In ->
+         let toks = match_token toks Tok_In in
+         let toks, expr2 = parse_expr toks in
+         toks, Let (var, recursive, expr1, expr2)
+       | _ -> toks, Let (var, recursive, expr1, ID var))
+    | Tok_If ->
+      let toks = match_token toks Tok_If in
+      let toks, cond = parse_expr toks in
+      let toks = match_token toks Tok_Then in
+      let toks, expr1 = parse_expr toks in
+      let toks = match_token toks Tok_Else in
+      let toks, expr2 = parse_expr toks in
+      toks, If (cond, expr1, expr2)
+    | Tok_Fun ->
+      let toks = match_token toks Tok_Fun in
+      let toks, var = parse_id toks in
+      let toks = match_token toks Tok_Arrow in
+      let toks, expr = parse_expr toks in
+      toks, Fun (var, expr)
+    | _ -> parse_or toks
 
-let rec parse_mutop toks = failwith "unimplemented"
+and parse_recursion toks =
+  match lookahead toks with
+  | Some Tok_Rec ->
+    let toks = match_token toks Tok_Rec in
+    toks, true
+  | _ -> toks, false
+
+and parse_or toks =
+  let toks, expr = parse_and toks in
+  match lookahead toks with
+  | Some Tok_Or ->
+    let toks = match_token toks Tok_Or in
+    let toks, expr2 = parse_or toks in
+    toks, Binop (Or, expr, expr2)
+  | _ -> toks, expr
+
+and parse_and toks =
+  let toks, expr = parse_equality toks in
+  match lookahead toks with
+  | Some Tok_And ->
+    let toks = match_token toks Tok_And in
+    let toks, expr2 = parse_and toks in
+    toks, Binop (And, expr, expr2)
+  | _ -> toks, expr
+
+and parse_equality toks =
+  let toks, expr = parse_relational toks in
+  match lookahead toks with
+  | Some Tok_Equal ->
+    let toks = match_token toks Tok_Equal in
+    let toks, expr2 = parse_equality toks in
+    toks, Binop (Equal, expr, expr2)
+  | Some Tok_NotEqual ->
+    let toks = match_token toks Tok_NotEqual in
+    let toks, expr2 = parse_equality toks in
+    toks, Binop (NotEqual, expr, expr2)
+  | _ -> toks, expr
+
+and parse_relational toks =
+  let toks, expr = parse_additive toks in
+  match lookahead toks with
+  | Some Tok_Less ->
+    let toks = match_token toks Tok_Less in
+    let toks, expr2 = parse_additive toks in
+    toks, Binop (Less, expr, expr2)
+  | Some Tok_Greater ->
+    let toks = match_token toks Tok_Greater in
+    let toks, expr2 = parse_additive toks in
+    toks, Binop (Greater, expr, expr2)
+  | Some Tok_LessEqual ->
+    let toks = match_token toks Tok_LessEqual in
+    let toks, expr2 = parse_additive toks in
+    toks, Binop (LessEqual, expr, expr2)
+  | Some Tok_GreaterEqual ->
+    let toks = match_token toks Tok_GreaterEqual in
+    let toks, expr2 = parse_additive toks in
+    toks, Binop (GreaterEqual, expr, expr2)
+  | _ -> toks, expr
+
+and parse_additive toks =
+  let toks, expr = parse_multiplicative toks in
+  match lookahead toks with
+  | Some Tok_Add ->
+    let toks = match_token toks Tok_Add in
+    let toks, expr2 = parse_additive toks in
+    toks, Binop (Add, expr, expr2)
+  | Some Tok_Sub ->
+    let toks = match_token toks Tok_Sub in
+    let toks, expr2 = parse_additive toks in
+    toks, Binop (Sub, expr, expr2)
+  | _ -> toks, expr
+
+and parse_multiplicative toks =
+  let toks, expr = parse_concat toks in
+  match lookahead toks with
+  | Some Tok_Mult ->
+    let toks = match_token toks Tok_Mult in
+    let toks, expr2 = parse_multiplicative toks in
+    toks, Binop (Mult, expr, expr2)
+  | Some Tok_Div ->
+    let toks = match_token toks Tok_Div in
+    let toks, expr2 = parse_multiplicative toks in
+    toks, Binop (Div, expr, expr2)
+  | _ -> toks, expr
+
+and parse_concat toks =
+  let toks, expr = parse_unary toks in
+  match lookahead toks with
+  | Some Tok_Concat ->
+    let toks = match_token toks Tok_Concat in
+    let toks, expr2 = parse_concat toks in
+    toks, Binop (Concat, expr, expr2)
+  | _ -> toks, expr
+
+and parse_unary toks =
+  match lookahead toks with
+  | Some Tok_Not ->
+    let toks = match_token toks Tok_Not in
+    let toks, expr = parse_unary toks in
+    toks, Not expr
+  | _ -> parse_app toks
+
+and parse_app toks =
+  let toks, expr = parse_select toks in
+  match lookahead toks with
+  | Some Tok_LParen ->
+    let toks = match_token toks Tok_LParen in
+    let toks, arg = parse_expr toks in
+    let toks = match_token toks Tok_RParen in
+    toks, App (expr, arg)
+  | _ -> toks, expr
+
+and parse_select toks =
+  let toks, expr = parse_primary toks in
+  match lookahead toks with
+  | Some Tok_Dot ->
+    let toks = match_token toks Tok_Dot in
+    let toks, lab = parse_id toks in
+    toks, Select (Lab lab, expr)
+  | _ -> toks, expr
+
+and parse_primary toks =
+  match lookahead toks with
+  | Some Tok_Int i ->
+    let toks = match_token toks (Tok_Int i) in
+    toks, Int i
+  | Some Tok_Bool b ->
+    let toks = match_token toks (Tok_Bool b) in
+    toks, Bool b
+  | Some Tok_String s ->
+    let toks = match_token toks (Tok_String s) in
+    toks, String s
+  | Some Tok_ID id ->
+    let toks, id = parse_id toks in
+    toks, ID id
+  | Some Tok_LParen ->
+    let toks = match_token toks Tok_LParen in
+    let toks, expr = parse_expr toks in
+    let toks = match_token toks Tok_RParen in
+    toks, expr
+  | Some Tok_LCurly ->
+    parse_record toks
+  | _ -> raise (InvalidInputException "Expected primary expression")
+
+and parse_record toks =
+  let toks = match_token toks Tok_LCurly in
+  match lookahead toks with
+  | Some Tok_RCurly -> 
+    let toks = match_token toks Tok_RCurly in
+    toks, Record []
+  | _ ->
+    let toks, body = parse_record_body toks in
+    let toks = match_token toks Tok_RCurly in
+    toks, Record body
+
+and parse_record_body toks =
+  let toks, lab = parse_id toks in
+  let toks = match_token toks Tok_Equal in
+  let toks, expr = parse_expr toks in
+  match lookahead toks with
+  | Some Tok_Semi ->
+    let toks = match_token toks Tok_Semi in
+    let toks, body = parse_record_body toks in
+    toks, (Lab lab, expr) :: body
+  | _ -> toks, [(Lab lab, expr)]
+
+and parse_id toks =
+  match lookahead toks with
+  | Some (Tok_ID id) ->
+    let toks = match_token toks (Tok_ID id) in
+    toks, id
+  | _ -> raise (InvalidInputException "Expected identifier")
+
+let rec parse_mutop toks =
+  match lookahead toks with
+  | None -> raise (InvalidInputException "Expected mutop")
+  | Some Tok_Def -> parse_def_mutop toks
+  | Some Tok_DoubleSemi -> match_token toks Tok_DoubleSemi, NoOp
+  | _ -> parse_expr_mutop toks
+
+and parse_def_mutop toks =
+  let toks = match_token toks Tok_Def in
+  let toks, var = parse_id toks in
+  let toks = match_token toks Tok_Equal in
+  let toks, expr = parse_expr toks in
+  let toks = match_token toks Tok_DoubleSemi in
+  toks, Def (var, expr)
+
+and parse_expr_mutop toks =
+  let toks, expr = parse_expr toks in
+  let toks = match_token toks Tok_DoubleSemi in
+  toks, Expr expr
