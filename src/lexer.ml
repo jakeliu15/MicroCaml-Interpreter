384ae7c6cccd7082b6de99f77923803564bcbc7c
jakeliu15 <91630668+jakeliu15@users.noreply.github.com>
2024-04-06 23:26:49 -0400

    f

--- src/lexer.ml
+++ src/lexer.ml
@@ -17,7 +17,7 @@ let tokenize input =
     else
       let input = ignore_spaces input in  
 
-      if string_match (regexp "^[0-9]+-") input 0 then
+      if string_match (regexp "^\\([0-9]+-\\)") input 0 then
         let matched = String.sub (matched_group 1 input) 0 1 in
         let n = int_of_string matched in
         helper (ignore_spaces (string_after input (String.length matched + 1))) (Tok_Sub :: Tok_Int n :: tokens) 
